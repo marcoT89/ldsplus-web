1f01a93bcbd2b18169cec204b70efa8763a0fc7d
Marco Tulio de Avila Santos <marcotulio.avila@gmail.com>
2018-06-16 20:03:35 -0300

    Adding transition component ListTransition

---
+++ components/transitions/ListTransition.vue
@@ -0,0 +1,53 @@
+<template lang="pug">
+  transition-group(
+    @before-enter="beforeEnter",
+    @after-leave="afterLeave"
+    @leave="leave",
+    @enter="enter")
+    slot
+</template>
+
+<script>
+export default {
+  methods: {
+    beforeEnter(el) {
+      console.log('beforeEnter')
+      this.$velocity(
+        el,
+        { opacity: 0, translateX: '30px' }
+      )
+    },
+
+    afterLeave(el) {
+      // console.log('afterLeave');
+
+      // this.$velocity(
+      //   el,
+      //   { opacity: 1 },
+      // )
+    },
+
+    enter(el, done) {
+      console.log('enter');
+
+      const delay = el.dataset.index * 80
+      this.$velocity(
+        el,
+        { opacity: 1, translateX: '0px' },
+        { complete: done, delay: delay, duration: 'normal' }
+      )
+    },
+
+    leave(el, done) {
+      console.log('leave');
+
+      const delay = 80
+      this.$velocity(
+        el,
+        { opacity: 0, translateX: '-30px' },
+        { complete: done, delay, duration: 'normal' }
+      )
+    }
+  }
+}
+</script>
