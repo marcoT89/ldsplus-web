ed75585d400c423836410582e17a4db88eba9548
Marco Tulio de Avila Santos <marcotulio.avila@gmail.com>
2018-07-15 00:01:24 -0300

    Make initial events list page

---
+++ components/shared/FabButton.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+  button.fab-button(v-waves.button.light.float)
+    slot
+</template>
+
+<style lang="sass" scoped>
+.fab-button
+  position: fixed
+  bottom: 1rem
+  right: 1rem
+  border-radius: 50px
+  padding: 0.75rem 1.2rem
+
+</style>
+
+
+<script>
+export default {};
+</script>
--- pages/events.vue
+++ pages/events.vue
@@ -1,5 +1,43 @@
 <template lang="pug">
   section.page
     h4.bg-primary.py-2.mb-0.text-white
-      .container Eventos
+      .container Events
+    div.page-content.container
+      .form-group
+        input.search.form-control(placeholder="Search for events...")
+
+      .row.event-entry.d-flex.align-items-center.mb-0.px-2(v-for="n of 31", v-waves)
+        .p-2.text-center
+          h3.event-day.mb-0 {{ n }}
+          .event-month jun
+        .flex-fill.p-2
+          .event-title Nome do Evento
+          .event-time às 19h
+        small.p-2.text-uppercase.text-success
+          span inscrito
+          i.fa.fa-check.ml-1
+    fab-button.btn.btn-primary.btn-lg.z-depth-2
+      i.fa.fa-plus
+
 </template>
+
+<style lang="sass" scoped>
+@import '../assets/design-bootstrap'
+
+.event-entry
+  .event-title
+    color: $primary
+  .event-time
+    font-size: 12px
+    color: $secondary
+  .event-day
+    position: relative
+    top: 3px
+    font-weight: bold
+    width: 2rem
+  .event-month
+    font-size: 12px
+    position: relative
+    top: -2px
+    text-transform: uppercase
+</style>
